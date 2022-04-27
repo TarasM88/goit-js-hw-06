8b78d0a07c170abfb3993fb65af50bd7ee05f6c7
Taras Makuts <makuts1888@gmail.com>
2022-04-27 19:15:38 +0300

    js.hw.6

---
+++ js/task-02.js
@@ -0,0 +1,17 @@
+const ingredients = [
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
+  ];
+  
+  const arrayOfListElements = ingredients.map(ingredient => {
+    const listItemEl = document.createElement('li');
+    listItemEl.classList.add('item');
+    listItemEl.textContent = ingredient;
+    return listItemEl;
+  });
+  
+  document.querySelector('#ingredients').append(...arrayOfListElements);
